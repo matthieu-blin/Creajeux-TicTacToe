ad9ce161e42422625fa6a2d76d4e3c8b185bbfd4
matthieu-blin <matth.blin@gmail.com>
2023-09-15 16:28:20 +0200

    Analysis + Protocol (q1-2-3)

---
+++ Assets/TicTacToe/Code/Protocol.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protocol
+{
+    public const int GAME_START_PLAYER = 1; //<int> id du player : bleu = 0, rouge = 1
+    public  const int GAME_PLAYER_MOVE = 2; //<int id><int cell index>
+}
